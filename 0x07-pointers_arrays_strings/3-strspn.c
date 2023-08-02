afa691d599a81c14a73ab41f4a193937ab3215a6
Vyktr <younes-4561@hotmail.fr>
2023-08-02 03:30:48 +0100

    33

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,15 +6,14 @@
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
+unsigned int n = 0;
+int r;
+while (*s)
 {
-	unsigned int n = 0;
-		int r;
-			while (*s)
-				{
-							for (r = 0; accept[r]; r++)
-									{
-													if (*s == accept[r])
-																{
+	for (r = 0; accept[r]; r++)
+	{
+		if (*s == accept[r])
+		{
 																					n++;
 																									break;
 																				}
